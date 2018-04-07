f89b19bb93b89f8547516546ec96fdc6222126af
Greg Laabs <OverloadUT@gmail.com>
2018-04-06 20:33:09 -0700

    First version

---
+++ bridge.py
@@ -0,0 +1,25 @@
+from Reader import Reader
+import time
+import os
+import socket
+from requests import post
+
+reader = Reader()
+
+url = os.environ['HASSURL'] + '/api/events/rfid_scan'
+headers = {'x-ha-access': os.environ['HASSKEY'],
+           'content-type': 'application/json'}
+
+print('Reader is ready')
+
+while True:
+    card = reader.readCard()
+    try:
+        event_data = '{"scanner": "' + socket.gethostname() + '", "card_id": "'+card+'"}'
+        print('Read card:', card, event_data)
+        response = post(url, headers=headers, data=event_data)
+        print(response.text)
+        time.sleep(0.2)
+    except OSError as e:
+        print('Execution failed:', e)
+        time.sleep(0.2)
